0d443d9e50500c068bbf5a124cfbd05ea818a28e
weisk <2716537+weisk@users.noreply.github.com>
2024-02-12 18:16:59 +0100

    tree of checkboxes. time is up!

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,54 @@
 import React from 'react';
-import {
-  Button,
-} from 'antd';
+import { Layout, Flex } from 'antd';
 
 import JurisdictionSelector from '../JurisdictionSelector/JurisdictionSelector';
-import './App.css';
+
+const { Header, Footer, Content } = Layout;
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: '#4169e1',
+};
+
+const contentStyle: React.CSSProperties = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  // color: '#fff',
+  backgroundColor: '#f8f9fa',
+};
+
+const footerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#4169e1',
+};
+
+const layoutStyle = {
+  borderRadius: 8,
+  overflow: 'hidden',
+  width: 'calc(50% - 8px)',
+  maxWidth: 'calc(50% - 8px)',
+};
+
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Button type="primary">Click</Button>
+    <Flex gap="middle" wrap="wrap">
+      <Layout style={layoutStyle}>
+        <Header style={headerStyle}>Browse Jurisdictions</Header>
 
-      <JurisdictionSelector />
-    </div>
+        <Content style={contentStyle}>
+          <JurisdictionSelector />
+        </Content>
+
+        <Footer style={footerStyle}>vLex</Footer>
+      </Layout>
+     </Flex>
   );
 };
 
