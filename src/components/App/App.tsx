81d69f6c1b7c78a86cea5717f774c2133c501ecd
weisk <2716537+weisk@users.noreply.github.com>
2024-02-12 16:54:28 +0100

    create JurisdictionSelector component. Add types to the provided fake api, and test some calls.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Button } from 'antd';
+import {
+  Button,
+} from 'antd';
 
+import JurisdictionSelector from '../JurisdictionSelector/JurisdictionSelector';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Button type="primary">Click</Button>
+
+      <JurisdictionSelector />
     </div>
   );
 };
